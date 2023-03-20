1567e6cf0e51125725b57927b807b73ee00d2406
n2oblife <zackanit@gmail.com>
2023-03-20 14:28:58 +0100

    test vgg16

--- export_to_remote/efficientNet.py
+++ export_to_remote/efficientNet.py
@@ -76,6 +76,104 @@ transform_test = transforms.Compose([
 # Utiliser le dropout pour éviter un gros overfitting et le mixup
 ##
 
+# ---------------------------------------------------------------
+# Definition of the model
+
+class VGG16(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG16, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            #nn.Linear(7*7*512, 4096),
+            nn.Linear(512, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, num_classes))
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+    
+# --------------------------------------------------------------
+
+
 ## Hyperparameters
 num_classes = 100
 num_epochs = 90
@@ -100,9 +198,11 @@ num_train_examples = len(c100train)
 num_samples_subset = 15000
 
 # Model definition
-model = EfficientNet.from_name('efficientnet-b1', num_classes=num_classes).to(device)
-model_dir = './models/efficientnet-b1_base_cifar100.pt'
-model_dir_early = './models/efficientnet-b1_base_cifar100_early.pt'
+model_name = 'vgg16'
+#model = EfficientNet.from_name('efficientnet-b1', num_classes=num_classes).to(device)
+model = model = VGG16(num_classes).to(device)
+model_dir = './models/'+model_name+'_base_cifar100.pt'
+model_dir_early = './'+model_name+'_base_cifar100_early.pt'
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -182,7 +282,7 @@ for epoch in range(num_epochs):
 
     train_losses.append(running_loss / total)
     train_acc.append(100*correct/total)
-    print('\n'+f'Train Loss : {train_losses[-1]:.4f}, Train accuracy : {train_acc[-1]:.4f}')
+    print('\n'+f'Train Loss : {train_losses[-1]:.4f} , Train accuracy : {train_acc[-1]:.4f}')
 
 
     with torch.no_grad():
@@ -215,7 +315,7 @@ for epoch in range(num_epochs):
         # del images, labels, outputs
         # torch.cuda.empty_cache()
 
-    print(f'Validation loss: {val_losses[-1]:.4f}, Validation accuracy: {val_acc[-1]:.4f}')
+    print(f'Validation loss: {val_losses[-1]:.4f} , Validation accuracy: {val_acc[-1]:.4f}')
     print('---------------------------------------')
 
     # Early stopping in case of overfitting
@@ -233,8 +333,8 @@ torch.save(model.state_dict(), model_dir)
 # ------------------------------------------
 # save the metrics
 
-my_file = "./models/efficientnet-b1_base_cifar100.txt"
-file_dir = "./models/efficientnet-b1_base_cifar100.png"
+my_file = './models/'+model_name+'_base_cifar100.txt'
+file_dir = './models/'+model_name+'_base_cifar100.png'
 
 with open(my_file, 'wb') as f:
     pickle.dump(train_losses, f)
